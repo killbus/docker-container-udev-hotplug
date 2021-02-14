ab696bd86e70b54b2e64ffe3d178277a9e9b353c
killbus <killbus@users.noreply.github.com>
2021-02-14 15:31:31 +0800

    initial mounts

--- scripts/idle.sh
+++ scripts/idle.sh
@@ -3,5 +3,11 @@
 # dump system environment
 env >/tmp/environmentfile
 
+# initlal mounts
+usb_storage_devices=$(lsblk | grep -oP "sd[a-z][0-9]" | awk '{print "/dev/"$1}')
+for device in ${usb_storage_devices[@]}; do
+  /usr/src/scripts/mount.sh "$device"
+done
+
 # Just an infinite loop to prevent container from exiting
 balena-idle
