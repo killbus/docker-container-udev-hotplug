335b190762cf368b2e641e837c4e8f10bee7882f
killbus <killbus@users.noreply.github.com>
2021-03-12 12:34:47 +0800

    rollback to prefer UUID

--- scripts/unmount.sh
+++ scripts/unmount.sh
@@ -20,25 +20,25 @@ fi
 # Get required device information
 ID_BUS=${ID_BUS:=$(udevadm info -n $DEVNAME | awk -F "=" '/ID_BUS/{ print $2 }')}
 ID_FS_TYPE=${ID_FS_TYPE:=$(udevadm info -n $DEVNAME | awk -F "=" '/ID_FS_TYPE/{ print $2 }')}
-# ID_FS_UUID_ENC=${ID_FS_UUID_ENC:=$(udevadm info -n $DEVNAME | awk -F "=" '/ID_FS_UUID_ENC/{ print $2 }')}
-ID_PART_ENTRY_UUID=${ID_PART_ENTRY_UUID:=$(udevadm info -n $DEVNAME | awk -F "=" '/ID_PART_ENTRY_UUID/{ print $2 }')}
+ID_FS_UUID_ENC=${ID_FS_UUID_ENC:=$(udevadm info -n $DEVNAME | awk -F "=" '/ID_FS_UUID_ENC/{ print $2 }')}
+# ID_PART_ENTRY_UUID=${ID_PART_ENTRY_UUID:=$(udevadm info -n $DEVNAME | awk -F "=" '/ID_PART_ENTRY_UUID/{ print $2 }')}
 ID_FS_LABEL_ENC=${ID_FS_LABEL_ENC:=$(udevadm info -n $DEVNAME | awk -F "=" '/ID_FS_LABEL_ENC/{ print $2 }')}
 
-if [[ -z $ID_BUS || -z $ID_FS_TYPE || -z $ID_PART_ENTRY_UUID || -z $ID_FS_LABEL_ENC ]]; then
+if [[ -z $ID_BUS || -z $ID_FS_TYPE || -z $ID_FS_UUID_ENC || -z $ID_FS_LABEL_ENC ]]; then
   info "Could not get device information: $DEVNAME"
   exit 1
 fi
 
 # Construct the mount point path
-MOUNT_POINT=/mnt/storage-$ID_BUS-$ID_FS_LABEL_ENC-$ID_PART_ENTRY_UUID
+MOUNT_POINT=/mnt/storage-$ID_BUS-$ID_FS_LABEL_ENC-$ID_FS_UUID_ENC
 
 # Unmount device
 if findmnt -rno SOURCE,TARGET $DEVNAME >/dev/null; then
   info "Unmounting device - Source: $DEVNAME - Destination: $MOUNT_POINT"
   umount -f $MOUNT_POINT
   rmdir $MOUNT_POINT
-  action_containers $ID_PART_ENTRY_UUID >/proc/1/fd/1 2>/proc/1/fd/2
-  action_services $ID_PART_ENTRY_UUID >/proc/1/fd/1 2>/proc/1/fd/2
+  action_containers $ID_FS_UUID_ENC >/proc/1/fd/1 2>/proc/1/fd/2
+  action_services $ID_FS_UUID_ENC >/proc/1/fd/1 2>/proc/1/fd/2
 else
   info "No mount point found for device $DEVNAME."
 fi
